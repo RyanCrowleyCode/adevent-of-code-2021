a76e0b7677f5cfdafb5fd8b195530e21601f23b4
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-09 10:55:12 -0600

    completed puzzle_2 for day 6

--- day_six/puzzle_1.rb
+++ day_six/puzzle_1.rb
@@ -1,8 +1,46 @@
 ################ Getting the data ##########################
+file = File.read("example.csv").split(",")
 
+def convert_to_int(file)
+    new_file = []
+    file.each do |number_string|
+        new_file.push(number_string.to_i)
+    end
+    new_file
+end
+
+file = convert_to_int(file)
 
 ################ Solving the problem ##########################
+@fish = file.dup
 
+def multiply_fish_for_days(days)
+    (days).times do
+        mutiply_fish_one_day
+    end
+end
+
+def mutiply_fish_one_day
+    new_fish = []
+    eights = 0
+    @fish.each do | timer |
+        if timer > 0
+            new_fish.push(timer -1)
+        else
+            new_fish.push(6)
+            eights += 1
+        end
+    end
+    (eights).times do
+        new_fish.push(8)
+    end
+    @fish = new_fish
+end
+
+def count_fish
+    @fish.length
+end
 
 ################ Showing the Answer ##########################
-
+multiply_fish_for_days(80)
+puts count_fish
--- day_six/puzzle_2.rb
+++ day_six/puzzle_2.rb
@@ -1,8 +1,71 @@
 ################ Getting the data ##########################
+file = File.read("day_six.csv").split(",")
 
+def convert_to_int(file)
+    new_file = []
+    file.each do |number_string|
+        new_file.push(number_string.to_i)
+    end
+    new_file
+end
+
+@fish_hash = Hash.new
+@fish_hash["0"] = 0
+@fish_hash["1"] = 0
+@fish_hash["2"] = 0
+@fish_hash["3"] = 0
+@fish_hash["4"] = 0
+@fish_hash["5"] = 0
+@fish_hash["6"] = 0
+@fish_hash["7"] = 0
+@fish_hash["8"] = 0
+
+def add_file_to_hash(file)
+    file.each do |number_string|
+        @fish_hash[number_string] += 1
+    end
+end
+
+add_file_to_hash(file)
+puts @fish_hash
+
+file = convert_to_int(file)
 
 ################ Solving the problem ##########################
+@fish = file.dup
 
+def multiply_fish_for_days_hash(days)
+    days.times do 
+        new_fish = @fish_hash["0"]
+        @fish_hash["0"] = @fish_hash["1"]
+        @fish_hash["1"] = @fish_hash["2"]
+        @fish_hash["2"] = @fish_hash["3"]
+        @fish_hash["3"] = @fish_hash["4"]
+        @fish_hash["4"] = @fish_hash["5"]
+        @fish_hash["5"] = @fish_hash["6"]
+        @fish_hash["6"] = @fish_hash["7"]
+        @fish_hash["7"] = @fish_hash["8"]
+        @fish_hash["8"] = new_fish
+        @fish_hash["6"] += new_fish
+    end
+end
+
+def count_fish(days)
+    count = 0
+    @fish.each do | fish |
+       count += multiply_fish_for_days(days, fish)
+    end
+    count
+end
+
+def count_fish_hash
+    count = 0
+    @fish_hash.each_value do |value|
+        count += value
+    end
+    count
+end
 
 ################ Showing the Answer ##########################
-
+multiply_fish_for_days_hash(256)
+puts count_fish_hash
