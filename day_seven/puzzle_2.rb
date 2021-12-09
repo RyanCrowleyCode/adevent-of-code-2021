ab19bdce908913bef033690c39b2eb2dbf2aa5a7
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-09 14:22:32 -0600

    refactored puzzle_2 to account for floor and ceiling averages

--- day_seven/puzzle_2.rb
+++ day_seven/puzzle_2.rb
@@ -1,7 +1,47 @@
 ################ Getting the data ##########################
-file = File.read("example.csv")
+file = File.read("example.csv").split(",")
 
-################ Solving the problem ##########################
+def convert_to_int(file)
+    numbers = []
+    file.each do |number|
+        numbers.push(number.to_i)
+    end
+    numbers
+end
+
+file = convert_to_int(file)
+
+################ Solving the problem ########################## 
+
+def get_average(list)
+    (list.reduce(:+).to_f / list.size)
+end
+
+def calculate_fuel(list)
+    average = get_average(list)
+    fuel_cost_floor = 0
+    fuel_cost_ceil = 0
+
+    list.each do |position|
+        floor_gap = (position - average.floor).abs
+        ceil_gap = (position - average.ceil).abs
+
+        fuel_cost_floor += calculate_incrementing_fuel(floor_gap)
+        fuel_cost_ceil += calculate_incrementing_fuel(ceil_gap)
+    end
+    [fuel_cost_floor, fuel_cost_ceil].min
+end
+
+def calculate_incrementing_fuel(number)
+    fuel = 0
+    i = number
+    while i > 0 
+        fuel += i
+        i -= 1
+    end
+    fuel
+end
 
 
 ################ Showing the Answer ##########################
+puts calculate_fuel(file)
