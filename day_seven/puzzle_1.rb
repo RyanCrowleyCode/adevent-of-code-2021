81e3c43013ec1596ab26847a5147ae6ac26211a0
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-09 13:13:33 -0600

    puzzle_1 day_seven complete

--- day_seven/puzzle_1.rb
+++ day_seven/puzzle_1.rb
@@ -1,7 +1,32 @@
 ################ Getting the data ##########################
-file = File.read("example.csv")
+file = File.read("day_seven.csv").split(",")
 
-################ Solving the problem ##########################
+def convert_to_int(file)
+    numbers = []
+    file.each do |number|
+        numbers.push(number.to_i)
+    end
+    numbers
+end
+
+file = convert_to_int(file).sort
+################ Solving the problem ########################## 
+
+def get_middle(list)
+    middle = list.length/2
+    list[middle]
+end
+
+def calculate_fuel(list)
+    middle = get_middle(list)
+    fuel_cost = 0
+
+    list.each do |position|
+        fuel_cost += (position - middle).abs
+    end
+    fuel_cost
+end
 
 
 ################ Showing the Answer ##########################
+puts calculate_fuel(file)
