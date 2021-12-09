b505e53b93cadb3590f1d96b0ff1e1f8137e13e0
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-09 14:32:39 -0600

    setup for day eight

---
+++ day_eight/puzzle_1.rb
@@ -0,0 +1,9 @@
+################ Getting the data ##########################
+file = File.read("example.csv")
+
+################ Solving the problem ########################## 
+
+
+
+
+################ Showing the Answer ##########################
