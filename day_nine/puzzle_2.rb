63b82a0a631305d6fc31e8e602e5622ee520fbfc
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-17 10:56:32 -0600

    setting up files for day_nine

---
+++ day_nine/puzzle_2.rb
@@ -0,0 +1,8 @@
+################ Getting the data ##########################
+file = File.read("example.csv")
+
+################ Solving the problem ########################## 
+
+
+################ Showing the Answer ##########################
+
