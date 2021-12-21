d8fb3a405b58faf196bde9cc04f6772f0c5f97d2
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-21 16:50:38 -0600

    create files for day eleven

---
+++ day_eleven/puzzle_1.rb
@@ -0,0 +1,9 @@
+################ Getting the data ##########################
+file = File.read("day_ten.csv")
+
+
+################ Solving the problem ########################## 
+
+
+################ Showing the Answer ##########################
+
