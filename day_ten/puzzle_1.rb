b96d21eab06a87705064fb4046ab0ac5ad8b6cee
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-12-21 14:24:28 -0600

    setup for day ten

---
+++ day_ten/puzzle_1.rb
@@ -0,0 +1,10 @@
+################ Getting the data ##########################
+file = File.read("example.csv")
+
+
+################ Solving the problem ########################## 
+
+
+
+################ Showing the Answer ##########################
+
